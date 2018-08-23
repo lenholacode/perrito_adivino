b7193aa5c680395bfe7e99066f53b01a5a75466e
jazzcelaya <jazzcelaya@gmail.com>
2018-08-23 12:27:00 -0500

    button

--- react-client/src/components/List.jsx
+++ react-client/src/components/List.jsx
@@ -5,6 +5,9 @@ const List = (props) => (
   <div>
     <h4> Your Future </h4>
     There are { props.items.length } possible futures for you.
+    <h2>
+    <button>OH GRAN PERRO SABIO, DIME MI FUTURO</button>
+    </h2>
   </div>
 )
 
